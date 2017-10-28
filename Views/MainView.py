9e082044e56a2673ea46189fd13b76adc52286ee
Alejandro Mendoza <zippi180@gmail.com>
2017-10-28 00:25:03 -0500

    Added MainView class

--- Views/MainView.py
+++ Views/MainView.py
@@ -1,12 +1,17 @@
 from tkinter import Tk, Label, Canvas, N, S, E, W
 
+
 class MainView(Tk):
     class Constants:
         title = "Pizarra Magica"
         height = 900
         width = 1000
+        side_width = 200
+        side_height = 200
         center = N + S + E + W
-        color = 'salmon'
+        border_color = 'salmon'
+        magic_board_color = 'white'
+        margin_size = 200
 
         @classmethod
         def size (cls):
@@ -14,5 +19,17 @@ class MainView(Tk):
 
     def __init__(self):
         super().__init__()
-        self.title(self.Constants.title)
+
+        self.title('Pizarra Mágica')
         self.geometry(self.Constants.size())
+        self.minsize(width=self.Constants.width, height=self.Constants.height)
+        self.maxsize(width=self.Constants.width, height=self.Constants.height)
+
+        self.configure(bg=self.Constants.border_color)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+
+        self.__magic_board = Canvas(self, width=600, height=500, bg=self.Constants.magic_board_color)
+        self.__magic_board.grid(row=1, column=0)
+
