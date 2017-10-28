d2998b6999452fbdadf15cef886a0ab3c321625d
Carlos Hdz <carlos_hdzcastro1997@outlook.com>
2017-10-27 19:57:45 -0500

    Caracteristicas Pizarra

--- Views/MainViews.py
+++ Views/MainViews.py
@@ -1,8 +1,10 @@
-from tkinter import Tk, Canvas, Label, N, S, E, W
+from tkinter import Tk, Canvas, Button, N, S, E, W
 
 class MainView(Tk):
     class Constants():
         title = "Pizarra Mágica"
+        text_color_red = "Rojo"
+        text_color_green = "Verde"
         height = 600
         width = 800
         height_sketch = 500
@@ -21,16 +23,17 @@ class MainView(Tk):
         self.minsize(self.Constants.width, self.Constants.height)
         self.config(bg = "#DA5D16")
 
-        self.__label = Label(self)
+        self.__color_button = Button(self)
         self.__canvas = Canvas(self, width = self.Constants.width_sketch, height = self.Constants.height_sketch)
 
         self.__canvas.grid(column=0, sticky=self.Constants.center)
         self.__canvas.grid(column=2, sticky=self.Constants.center)
         self.__canvas.grid(row = 1, column = 1, sticky = self.Constants.center)
+
         self.columnconfigure(0, minsize = int((self.Constants.width - self.Constants.width_sketch)/2))
         self.columnconfigure(1, minsize= int((self.Constants.width - self.Constants.width_sketch)/2))
         self.columnconfigure(2, minsize= int((self.Constants.width - self.Constants.width_sketch)/2))
-        self.rowconfigure(0, minsize = int((self.Constants.height - self.Constants.height_sketch)/2))
+
         self.rowconfigure(1, minsize=int((self.Constants.height - self.Constants.height_sketch) / 2))
         self.rowconfigure(2, minsize=int((self.Constants.height - self.Constants.height_sketch) / 2))
 
