10b3bda6e07e21dc2675083418f02dcd7cfb9a9e
MaFer <fernandahonojosa502@gmail.com>
2017-10-28 09:06:48 -0500

    First commit. Added start view

---
+++ Views/MainView.py
@@ -0,0 +1,20 @@
+from tkinter import Tk, Canvas, Label, N, S, E, W
+
+class MainView(Tk):
+    class Constants:
+        title = "Pizarra magica"
+        heigth = 500
+        width = 600
+        center = N + S + E + W
+
+        @classmethod
+        def size (cls):
+            return "{}x{}".format(cls.width, cls.heigth)
+
+    def __init__(self):
+        super().__init__()
+        self.title(self.Constants.title)
+        self.geometry(self.Constants.size())
+
+
+
