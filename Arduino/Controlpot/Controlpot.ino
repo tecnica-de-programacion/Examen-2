5dd51867c0b2561fe80aa36cf1269cd434300ac8
gustavo santos <gustavo.santos.nunez@gmail.com>
2017-10-29 14:16:02 -0600

    Fixed arduino and Python files

--- Arduino/Controlpot/Controlpot.ino
+++ Arduino/Controlpot/Controlpot.ino
@@ -1,15 +1,33 @@
-int potenciometroX_pin=0; 
-int valor_potenciometroX=0;
-int potenciometroY_pin=1; 
-int valor_potenciometroY=0;
+int potenciometroX_pin=A0; 
+int potenciometroY_pin=A1; 
+int mapValueX=0;
+int mapValueY=0;
+
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop(){
-  valor_potenciometroX=analogRead(potenciometroX_pin);
-  Serial.println(valor_potenciometroX);
-  valor_potenciometroY=analogRead(potenciometroY_pin);
-  Serial.println(valor_potenciometroY);
+  int sensorValueX=analogRead(potenciometroX_pin);
+  sendDataX(sensorValueX);
+  
+  int sensorValueY=analogRead(potenciometroY_pin);
+  sendDataY(sensorValueY);
+
   delay(100);
 }
+void sendDataX( int Xvalue) {
+  mapValueX = map(Xvalue,0,1023,0,600);
+  Serial.print(Xvalue);
+  Serial.print(',');
+  Serial.print(mapValueX);
+  Serial.print(',');
+  }
+void sendDataY(int Yvalue) {
+  mapValueY = map(Yvalue,0,1023,0,500);
+  Serial.print(Yvalue);
+  Serial.print(',');
+  Serial.println(mapValueY);
+  
+}
+
