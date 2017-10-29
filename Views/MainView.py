b9aa5012ffdf2689fd007a72f7163ca038ad9084
LauraReynosa <laliz0197@gmail.com>
2017-10-29 11:41:03 -0600

    Added clean screen

--- Views/MainView.py
+++ Views/MainView.py
@@ -25,9 +25,10 @@ class MainView(Tk):
         def size(cls):
             return '{}x{}'.format(cls.width, cls.height)
 
-    def __init__(self, tap_button_handler = None):
+    def __init__(self, tap_button_handler = None, tap_space_handler = None):
         super().__init__()
         self.__tap_button_handler = tap_button_handler
+        self.__tap_space_handler = tap_space_handler
         self.title(self.Constants.title)
         self.geometry(self.Constants.size())
         self.minsize(self.Constants.width, self.Constants.height)
@@ -54,11 +55,20 @@ class MainView(Tk):
         if self.__tap_button_handler is None: return
         self.__tap_button_handler(color)
 
+    def __did_space_tap(self, space):
+        if self.__tap_space_handler is None: return
+        self.__tap_space_handler(space)
+
     def update_canvas(self, x, y):
         if self.__coordinates:
             self.__canvas.create_line(self.__coordinates[-2], self.__coordinates[-1], x, y, width = 2)
         self.__coordinates.append(x)
         self.__coordinates.append(y)
+        self.bind(self.Constants.event_space, self.__did_space_tap)
 
-
+    def clean_screen(self):
+        self.__canvas = Canvas(self, width=self.Constants.drawing_screen_width,
+                               height=self.Constants.drawing_screen_height)
+        self.__canvas.place(x=self.Constants.left_width,
+                            y=(self.Constants.height - self.Constants.drawing_screen_height) / 2)
 
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class MainApp():
         close_event = "WM_DELETE_WINDOW"
 
     def __init__(self):
-        self.__master = MainView(tap_button_handler = self.__did_button_tap)
+        self.__master = MainView(tap_button_handler = self.__did_button_tap, tap_space_handler = self.__did_space_tap)
         self.__port = serial.Serial(self.Constants.port, self.Constants.baud)
         self.__update_coordinate()
 
@@ -17,6 +17,9 @@ class MainApp():
 
     def __did_button_tap(self, color):
         self.__color = color
+
+    def __did_space_tap(self, event):
+        self.__master.clean_screen()
 
     def __update_coordinate(self):
         coordinates = self.__port.readline().decode()
@@ -28,7 +31,6 @@ class MainApp():
         coordinate_y = int(clean_values[1])
         coordinate_x = int(clean_values[3])
         self.__master.update_canvas(coordinate_x, coordinate_y)
-
 
 if __name__ == '__main__':
     app = MainApp()
