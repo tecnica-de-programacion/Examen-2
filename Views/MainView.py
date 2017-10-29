3c6d1494518881d56b67adacc2855d7bd1f52f49
Quero <quero@iMac.local>
2017-10-28 23:54:35 -0500

    Updated MainView

--- Views/MainView.py
+++ Views/MainView.py
@@ -1,4 +1,26 @@
 from tkinter import Tk, Canvas, Label, N, S, E, W
 
 class MainView(Tk):
-    pass
+
+    class Constants:
+
+        title = "Etch a Sketch 2017 /,,/"
+        canvas_height = 600
+        canvas_width = 500
+        center = N + S + E + W
+
+        screen_height = 1000
+        screen_width = 1000
+
+        @classmethod
+        def size(cls):
+            return "{}x{}".format(cls.screen_width, cls.screen_height)
+
+    def __init__(self):
+        super().__init__()
+        self.title(self.Constants.title)
+        self.geometry(self.Constants.size())
+
+        self.__canvas = Canvas(self, width = self.Constants.canvas_width, height = self.Constants.canvas_height)
+
+        self.__canvas.grid(row = 1 , column = 2, sticky = self.Constants.center)
