92896905f154553406f3dca3dfbdbdaf26914e82
Carlos Hdz <carlos_hdzcastro1997@outlook.com>
2017-10-29 11:45:04 -0600

    cambio de posicion de los botones

--- Views/MainViews.py
+++ Views/MainViews.py
@@ -2,19 +2,23 @@ from tkinter import Tk, Label, Canvas, Button, N, S, E, W
 
 class MainView(Tk):
     class Constants():
-        title = "Pizarra Mágica"
+        title = "Pizarra  Mágica"
         height = 600
         width = 800
         height_sketch = 500
         width_sketch = 600
         width_button = 5
         height_button = 2
-        position_black_button = 200
-        position_red_button = 300
-        position_green_button = 400
-        position_blue_button = 500
+        x_position_button = 30
+        position_black_button = 100
+        position_red_button = 200
+        position_green_button = 300
+        position_blue_button = 400
         center = N + S + E + W
+        label_position = N
         text_label = "Colores:"
+        size_text = 20
+        font = "Harlow Solid Italic"
         color_label = "#CA9494"
         black = "black"
         red = "red"
@@ -59,23 +63,26 @@ class MainView(Tk):
         self.rowconfigure(2, minsize=int((self.Constants.height - self.Constants.height_sketch) / 2))
 
         self.__black_button = Button(self, bg = self.Constants.black, width = self.Constants.width_button, height = self.Constants.height_button)
-        self.__black_button.place(x = self.Constants.position_black_button)
+        self.__black_button.place(x = self.Constants.x_position_button, y = self.Constants.position_black_button)
         self.__black_button.bind(self.Constants.event, self.__get_black_color)
 
         self.__red_button = Button(self, bg = self.Constants.red, width = self.Constants.width_button, height = self.Constants.height_button)
-        self.__red_button.place(x = self.Constants.position_red_button)
+        self.__red_button.place(x = self.Constants.x_position_button, y = self.Constants.position_red_button)
         self.__red_button.bind(self.Constants.event, self.__get_red_color)
 
         self.__green_button = Button(self, bg = self.Constants.green, width = self.Constants.width_button, height = self.Constants.height_button)
-        self.__green_button.place(x = self.Constants.position_green_button)
+        self.__green_button.place(x = self.Constants.x_position_button, y = self.Constants.position_green_button)
         self.__green_button.bind(self.Constants.event, self.__get_green_color)
 
         self.__blue_button = Button(self, bg = self.Constants.blue, width = self.Constants.width_button, height = self.Constants.height_button)
-        self.__blue_button.place(x = self.Constants.position_blue_button)
+        self.__blue_button.place(x = self.Constants.x_position_button, y = self.Constants.position_blue_button)
         self.__blue_button.bind(self.Constants.event, self.__get_blue_color)
 
+        self.__title_label = Label(self, text = self.Constants.title, bg = self.Constants.color_bg, font=(self.Constants.font, self.Constants.size_text), fg= self.Constants.color_label)
+        self.__title_label.grid(row = 0, column = 1, sticky = self.Constants.center)
+
         self.__label = Label(self, text = self.Constants.text_label, bg = self.Constants.color_label)
-        self.__label.grid(row = 0,  column = 0)
+        self.__label.grid(row = 1,  column = 0, sticky = self.Constants.label_position)
 
         self.__delete_text = Label(self, text = self.Constants.delete_text, bg = self.Constants.color_label)
         self.__delete_text.grid(row = 2, column = 1)
