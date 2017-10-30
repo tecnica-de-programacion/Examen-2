5f30e573930b26e45229916cb01f0c84e9401004
sergilgg <sergil51@hotmail.com>
2017-10-29 18:46:42 -0600

    Creacion menu de colores

--- Views/MainView.py
+++ Views/MainView.py
@@ -4,16 +4,20 @@ class MainView(Tk):
 
     class Constants:
         title = 'Pizarra mágica'
-        width = 1100
-        height = 750
-        effective_area_h = 500
+        width = 750
+        height = 600
         effective_area_w = 600
+        effective_area_h = 500
         c_red = '#FF0000'
         c_blue = '#0000FF'
         c_yellow = '#FFFF00'
         c_green = '#00FF00'
+        c_black = '#040404'
+        c_white = '#FDFEFE'
         c_window = '#155287'
         c_draw = '#9BECFA'
+        space_x = 100
+        space_y = 50
         center = N + S + E + W
 
         @classmethod
@@ -28,16 +32,46 @@ class MainView(Tk):
         self.config(bg = self.Constants.c_window)
         self.minsize(self.Constants.width, self.Constants.height)
         self.maxsize(self.Constants.width, self.Constants.height)
-
+        self.color = self.Constants.c_black
 
 
         self.__draw_screen = Canvas(self, bg=self.Constants.c_draw, width=self.Constants.effective_area_w, height=self.Constants.effective_area_h)
-        self.__draw_screen.grid(column = 3, row=2, sticky=self.Constants.center)
+        self.__draw_screen.place(x=self.Constants.space_x, y=self.Constants.space_y)
+
+        self.__barra_Menu = Menu(self)
+        self.__menu_Color = Menu(master=self.__barra_Menu)
+
+        self.__menu_Color.add_command(label='Rojo', command=self.red)
+        self.__menu_Color.add_command(label='Azul', command=self.blue)
+        self.__menu_Color.add_command(label='Amarillo', command=self.yellow)
+        self.__menu_Color.add_command(label='Verde', command=self.green)
+        self.__menu_Color.add_separator()
+        self.__menu_Color.add_command(label='Negro', command=self.black)
+        self.__menu_Color.add_command(label='Blanco', command=self.white)
+
+        self.__barra_Menu.add_cascade(label='Color', menu=self.__menu_Color)
+        self.config(menu=self.__barra_Menu)
 
 
-        for num in range(0,5):
-            self.rowconfigure(num, weight=True)
-            self.columnconfigure(num, weight=True)
+    def red(self):
+        self.color = self.Constants.c_red
+        print(self.color)
+    def blue(self):
+        self.color = self.Constants.c_blue
+        print(self.color)
+    def yellow(self):
+        self.color = self.Constants.c_yellow
+        print(self.color)
+    def green(self):
+        self.color = self.Constants.c_green
+        print(self.color)
+    def black(self):
+        self.color = self.Constants.c_black
+        print(self.color)
+    def white(self):
+        self.color = self.Constants.c_white
+        print(self.color)
+
 
 
 
--- main.py
+++ main.py
@@ -6,10 +6,18 @@ class MainApp():
 
     def __init__(self):
         self.__master = MainView()
+        self.__arduino = serial.Serial('/dev/tty.usbmodem1411', 115200)
+        self.__master.protocol("WM_DELETE_WINDOW", self.__on_closing)
 
 
     def run (self):
         self.__master.mainloop()
+
+
+
+    def __on_closing(self):
+        self.__arduino.close()
+        self.__master.destroy()
 
 
 if __name__ == '__main__':
