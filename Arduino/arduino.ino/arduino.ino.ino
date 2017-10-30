b59a3d18391b6e0133c14afd97fe50ff3be75f94
David <deyivid@live.com>
2017-10-29 19:13:12 -0600

    se borro el archivo README y se modifico el archivo.ino

--- Arduino/arduino.ino/arduino.ino.ino
+++ Arduino/arduino.ino/arduino.ino.ino
@@ -1,23 +1,26 @@
 
 int perillaX = A0;
 int perillaY = A1;
-int potval = 0;
-int mapval = 0;
+int mapvalH = 0;
+int mapvalY = 0;
 
 void setup(){
   Serial.begin(115200);
 }
 
 void loop(){
-  int perilla_Val = analogRead(perillaX);
-  sendData(perilla_Val);
+  int perilla_x_Val = analogRead(perillaX);
+  int perilla_y_Val = analogRead(perillaY);
+  sendData(perilla_x_Val, perilla_y_Val);
 }
 
-void sendData(int value){
-  mapval = map(value, 0, 1023, 0, 500);
-  Serial.print(value);
+void sendData(int value_X, int value_Y){
+  mapvalH = map(value_X, 0, 1023, 0, 600);
+  mapvalY = map(value_Y, 0, 1023, 0, 500);
+  Serial.print(value_X);
   Serial.print(",");
-  Serial.println(mapval);
+  Serial.print(value_Y);
+  Serial.print(",");
   delay(100);
 }
 
