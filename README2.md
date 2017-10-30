0963d14fb047509908f433aa9df007cd2c3cc425
rulsanz <raul.sanz.hedz@gmail.com>
2017-10-29 20:11:44 -0600

    Se agregaron los colores, al parecer ya esta termindo

--- README2.md
+++ README2.md
@@ -7,6 +7,7 @@ Aqui agregar alguna nota de funcionamiento del programa
 
 ## Camputas de funcionameinto
 Si no corre a la primera, vuelva a presionar Run hasta que corra, si funciona
+Algunas veces abre la ventana pero arduino genera un error debido a que arroja datos raros, por lo que se tiene que cerrar y darle correr de nuevo
 
 ## Gif de fucnionamiento
 Aqui agregar un gif del fucnionameinto completo, donde se vea aun usuario interactuando 
--- Views/MainView.py
+++ Views/MainView.py
@@ -1,16 +1,18 @@
-from tkinter import *
-#from tkinter import Tk, Canvas, Label, N, S, E, W, Frame
+from tkinter import Tk, Canvas, Label, N, S, E, W, Frame, Button
 from Models.Brain import Brain
 
 
 class MainView(Tk):
     class Constants:
         title = "Pizarra Mágica"
-        width = 900
-        height = 700
+        width = 520
+        height = 630
         height_pizarra = 600
         width_pizarra = 500
+        position = 601
         center = N + S + E + W
+        distance = 150
+        color_line = 'Black'
 
 
         @classmethod
@@ -25,7 +27,7 @@ class MainView(Tk):
         self.__frame = Frame(self)
         self.__frame.grid(column = 0, row = 0)
 
-        self.configure(background = 'blue')
+        self.configure(background = 'dark blue')
 
         self.__controller = Brain()
         self.maxsize(self.Constants.width,self.Constants.height)
@@ -35,7 +37,7 @@ class MainView(Tk):
         self.__canvas.create_rectangle(0, 0, self.Constants.width_pizarra, self.Constants.height_pizarra, fill = 'white')
         self.__label = Label(self.__frame, text = 'COLORES')
 
-        self.__label.grid(row=0, column=3, sticky = N + E)
+        self.__label.grid(row=5, column=0, sticky = N + W)
         self.grid_rowconfigure(0, weight = True)
         self.grid_columnconfigure(0, weight = True)
 
@@ -43,6 +45,14 @@ class MainView(Tk):
         self.__frame.bind("<space>", self.did_space_tap)
         self.__frame.focus_set()
         self.__frame.pack()
+
+        self.button_green = Button(self, text=" Green", font = ('Century Gothic',7), command = self.__green_color).place(x=self.Constants.distance,y=self.Constants.position)
+        self.Constants.distance += 70
+        self.button_red = Button(self, text="Red", font = ('Century Gothic',7), command = self.__red_color).place(x=self.Constants.distance,y=self.Constants.position)
+        self.Constants.distance += 50
+        self.button_blue = Button(self, text=" Bue", font = ('Century Gothic',7), command = self.__blue_color).place(x=self.Constants.distance,y=self.Constants.position)
+        self.Constants.distance += 50
+        self.button_black = Button(self, text="Black", font = ('Century Gothic',7), command = self.__dark_color).place(x=self.Constants.distance,y=self.Constants.position)
 
     def did_space_tap(self, event):
         #print("pressed")
@@ -54,5 +64,23 @@ class MainView(Tk):
 
     def draw_line (self, initial_x, initial_y, data):
         vector = self.__controller.handle_data(data)
-        self.__canvas.create_line(initial_x, initial_y, vector[0], vector[1])
+        self.__canvas.create_line(initial_x, initial_y, vector[0], vector[1], fill = self.Constants.color_line, width = 2)
         return vector
+
+    def __blue_color(self):
+        #print('blue')
+        self.Constants.color_line = "blue"
+
+    def __dark_color(self):
+        #print('dark')
+        self.Constants.color_line = "black"
+
+    def __green_color(self):
+        #print('green')
+        self.Constants.color_line = "green"
+
+    def __red_color(self):
+        #print('red')
+        self.Constants.color_line = "red"
+
+
