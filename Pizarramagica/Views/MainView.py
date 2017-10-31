baaf192f35c4eaf78bb6a4ce33ac3afbf4d79535
gustavo santos <gustavo.santos.nunez@gmail.com>
2017-10-30 20:32:30 -0600

    Creating the Buttons.py

--- Pizarramagica/Views/MainView.py
+++ Pizarramagica/Views/MainView.py
@@ -1,5 +1,4 @@
-from tkinter import Tk, Canvas, Button
-
+from tkinter import Tk, Canvas, Button,Label,PhotoImage
 
 class MainView(Tk):
     class Constants:
@@ -8,7 +7,8 @@ class MainView(Tk):
         widthV = 800
         heigthC =500
         widthC = 600
-        pencil_color="Black"
+
+
 
         @classmethod
         def size(cls):
@@ -21,24 +21,29 @@ class MainView(Tk):
         self.title(self.Constants.title)
         self.geometry(self.Constants.size())
         self.__new_position=[]
-        self.pencil_color= "blue"
+        self.__pencil_color= "Blue"
         self.__line = None
+        #self.__fondo = Label(self, image=self.Constants.the_board)
+        #self.__fondo.place(x=50, y=20)
         self.__canvas = Canvas(self, width = self.Constants.widthC , height=self.Constants.heigthC,bg="white")
         self.__canvas.place(x = 60,y = 30)
-        self.__botonA=Button(self ,text ="Blue",bg="blue",command= to_Blue(self))
+
+        self.__botonA=Button(self ,text ="Blue",bg="blue",command= to_Blue)
         self.__botonA.place(x = 700,y =30)
-        self.__botonB = Button(self, text="Black", bg="black",command= to_Black(self))
+        self.__botonB = Button(self, text="Black", bg="black",command= to_Black)
         self.__botonB.place(x = 3,y = 30)
-        self.__botonV= Button(self, text="Green", bg="green",command= to_Green(self))
+        self.__botonV= Button(self, text="Green", bg="green",command= to_Green)
         self.__botonV.place(x = 3,y = 120)
-        self.__botonR = Button(self, text="Red", bg="Red",command= to_Red(self))
+        self.__botonR = Button(self, text="Red", bg="Red",command= to_Red)
         self.__botonR.place(x = 700,y= 120)
 
 
 
+
     def update_canvas(self , Xvalue, Yvalue):
+
         if self.__new_position:
-            self.__line= self.__canvas.create_line(self.__new_position[0],self.__new_position[1], Xvalue, Yvalue ,width=5, fill= self.pencil_color)
+            self.__line= self.__canvas.create_line(self.__new_position[0],self.__new_position[1], Xvalue, Yvalue ,width=3,fill=self.__pencil_color)
             self.__new_position[0]= Xvalue
             self.__new_position[1] = Yvalue
         else:
@@ -47,13 +52,14 @@ class MainView(Tk):
 
 
 def to_Blue(self):
-    self.pencil_color = "Blue"
+    self.__pencil_color = "Blue"
 
 def to_Green(self):
-    self.pencil_color = "Green"
+    self.__pencil_color = "Green"
 
 def to_Red(self):
-    self.pencil_color= "Red"
+    self.__pencil_color = "Red"
 
 def to_Black(self):
-    self.pencil_color= "Black"
+        self.__pencil_color = "Black"
+
--- Pizarramagica/main.py
+++ Pizarramagica/main.py
@@ -1,4 +1,5 @@
 from Views.MainView import MainView
+from tkinter import PhotoImage , Label
 import serial
 from serial.tools import list_ports
 
@@ -8,17 +9,19 @@ class MainApp():
         baud = 115200
         close_event = "WM_DELETE_WINDOW"
 
+
     def __init__(self):
         for port in list_ports.comports(include_links=True):
             print(port.device, port.name, port.description)
-
         self.__master = MainView()
         self.__arduino = serial.Serial(self.Constants.port, self.Constants.baud)
         self.__master.protocol(self.Constants.close_event, self.__on_closing)
+
         self.__update_clock()
 
     def run(self):
         self.__master.mainloop()
+
 
     def __handle_data(self, data):
         clean_values = data.strip(' \n\r').split(",")
@@ -30,6 +33,7 @@ class MainApp():
         data = self.__arduino.readline().decode()
         self.__handle_data(data)
         self.__master.after(5, self.__update_clock)
+
 
     def __on_closing(self):
         self.__arduino.close()
