e2e40eca0e0598ae882f6e5abc1b4670397fa1e7
gustavo santos <gustavo.santos.nunez@gmail.com>
2017-10-31 21:31:51 -0600

    Deleting magic numbers

--- Pizarramagica/Views/MainView.py
+++ Pizarramagica/Views/MainView.py
@@ -4,11 +4,12 @@ from Views.Labels import BackGround
 class MainView(Tk):
     class Constants:
         title = "Pizarra magica"
-        heigthV = 1000
-        widthV = 1000
+        heigthV = 900
+        widthV = 800
         event_space = '<space>'
-        heigthC =500
+        heigthC = 500
         widthC = 600
+        widthPencil = 3
         Red = '#FF0000'
         Black = '#000000'
         Green = '#00BB00'
@@ -17,6 +18,7 @@ class MainView(Tk):
         @classmethod
         def size(cls):
             return "{}x{}".format(cls.widthV, cls.heigthV)
+
     class Images:
         Black_button = "assets/blackB2.png"
         Blue_button = "assets/blueB2.png"
@@ -24,16 +26,22 @@ class MainView(Tk):
         Red_button = "assets/redB.png"
         My_board = "assets/pi.png"
 
-
+    class Positions:
+        x_canv_and_backg = 100
+        y_backg = 18
+        x_black_blue = 13
+        y_canv_redB_blackB = 30
+        y_blue_gren = 400
+        x_red_green = 710
 
 
     class NewPencil:
         pencil_color = '#000000'
 
-    def __init__(self,tap_button_handler=None,tap_space_handler=None):
+    def __init__(self, tap_button_handler = None, tap_space_handler = None):
         super().__init__()
-        self.__tap_button_handler=tap_button_handler
-        self.__tap_space_handler=tap_space_handler
+        self.__tap_button_handler = tap_button_handler
+        self.__tap_space_handler = tap_space_handler
         self.title(self.Constants.title)
         self.geometry(self.Constants.size())
         self.minsize(self.Constants.widthV,self.Constants.heigthV)
@@ -45,23 +53,22 @@ class MainView(Tk):
         self.__line = None
 
         self.__fondo = PhotoImage(file = self.Images.My_board)
-
-        self.__board =BackGround(self,100,18,image=self.__fondo,width=600)
+        self.__board =BackGround(self, self.Positions.x_canv_and_backg, self.Positions.y_backg,image=self.__fondo, width = self.Constants.widthC)
 
         self.__canvas = Canvas(self, width = self.Constants.widthC , height=self.Constants.heigthC,bg="white")
-        self.__canvas.place(x = 100,y = 30)
+        self.__canvas.place( x = self.Positions.x_canv_and_backg , y = self.Positions.y_canv_redB_blackB)
 
         self.__blackBi = PhotoImage(file=self.Images.Black_button)
-        self.__black_button= ChangeColorButton(self ,13,30,self.Constants.Black,self.__blackBi,action=self.__did_button_tap)
+        self.__black_button= ChangeColorButton(self ,self.Positions.x_black_blue , self.Positions.y_canv_redB_blackB,self.Constants.Black,self.__blackBi,action=self.__did_button_tap)
 
         self.__blueBi=PhotoImage(file=self.Images.Blue_button)
-        self.__blue_button= ChangeColorButton(self ,13,400,self.Constants.Blue,self.__blueBi,action=self.__did_button_tap)
+        self.__blue_button= ChangeColorButton(self ,self.Positions.x_black_blue ,self.Positions.y_blue_gren,self.Constants.Blue,self.__blueBi,action=self.__did_button_tap)
 
         self.__redBi=PhotoImage(file=self.Images.Red_button)
-        self.__red_button= ChangeColorButton(self ,710,30,self.Constants.Red,self.__redBi,action=self.__did_button_tap)
+        self.__red_button= ChangeColorButton(self ,self.Positions.x_red_green,self.Positions.y_canv_redB_blackB,self.Constants.Red,self.__redBi,action=self.__did_button_tap)
 
         self.__greenBi=PhotoImage(file=self.Images.Green_button)
-        self.__green_button= ChangeColorButton(self ,710,400,self.Constants.Green,self.__greenBi,action=self.__did_button_tap)
+        self.__green_button= ChangeColorButton(self ,self.Positions.x_red_green,self.Positions.y_blue_gren,self.Constants.Green,self.__greenBi,action=self.__did_button_tap)
 
     def __did_button_tap(self,color):
         if self.__tap_button_handler is None: return
@@ -76,7 +83,7 @@ class MainView(Tk):
     def update_canvas(self , Xvalue, Yvalue):
 
         if self.__new_position:
-            self.__line= self.__canvas.create_line(self.__new_position[0],self.__new_position[1], Xvalue, Yvalue ,width=3,fill=self.NewPencil.pencil_color)
+            self.__line= self.__canvas.create_line(self.__new_position[0],self.__new_position[1], Xvalue, Yvalue ,width=self.Constants.widthPencil,fill=self.NewPencil.pencil_color)
             self.__new_position[0]= Xvalue
             self.__new_position[1] = Yvalue
         else:
