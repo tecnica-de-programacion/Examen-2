4fd292c888087e6071870de915bc89e050c5216b
OhtokaniHernandezAQ <antoniohernandezq.177@gmail.com>
2017-11-01 19:44:43 -0600

    Poniendo la pantalla de dibujo

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from tkinter import Tk, Button, Label, PhotoImage, Canvas, N, S, E, W
 
+class DrawScreen(Canvas):
+    class Constants:
+        drawing_screen_height = 500
+        drawing_screen_width = 600
+        color_bg = '#911e5d'
+
+    def __init__(self, master, drawing_screen_height, drawing_screen_width):
+        super().__init__(master)
+        self.__drawing_canvas = Canvas(self, width=self.Constants.drawing_screen_width,
+                               height=self.Constants.drawing_screen_height, bg=self.Constans.color_bg)
+        self.__drawing_canvas.grid(row = 1, column = 0)
+
 class ColorButton():
-    def __init__(self, master, column, color, file_path, action=None):
+    def __init__(self, master, column, color, action=None):
         self.__master = master
-        self.__color = color
-        self.__file_path = file_path
-        self.__action = action
         self.__column = column
+        self.__color = color
+        self.__action = action
+
+    def __press_button_tap(self):
+        if self.__action is None: return
+        self.__action(self.__color)
 
 class MainView(Tk):
     class Constants:
         title = 'Pizarra mágica'
-        drawing_screen_height = 500
-        drawing_screen_width = 600
         width = 900
         height = 700
         event_space = '<space>'
